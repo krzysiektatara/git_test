b1255836d9af4efb96bd2edc608e4bf93825963e
Krzysztof Tatara <krzysiektt@gmail.com>
2022-03-07 14:19:32 +0100

    commit git_test

---
+++ src/main/java/com/epam/sum_university/properties/Properties.java
@@ -0,0 +1,6 @@
+package com.epam.sum_university.properties;
+
+public final class Properties {
+    public static final String GIT_HUB_LOGIN = "krzysiektt@gmail.com";
+    public static final String GIT_HUB_PASS = "xxx";
+}
